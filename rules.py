9d44ace8b9023618ba0043bd485953720f1feff6
Adrian Martin <adrianmartinparra@gmail.com>
2012-11-21 20:19:49 +0100

    Updated rules.
    Added "bush_mosteller_update" and "incremental_learning_update".

--- rules.py
+++ rules.py
@@ -2,3 +2,9 @@
 
 def ant_system_update(rho, weight, increase):
     return (1 - rho) * weight + increase
+
+def bush_mosteller_update(alpha, weight, increase):
+    return alpha * weight + (1 - alpha) * increase
+
+def incremental_learning_update(delta, weight, increase):
+    return weight + delta * increase
